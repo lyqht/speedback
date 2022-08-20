d49a5bed5d10e1bc69fcff7d47b2ab02d98627ef
lyqht <estee.tsw@gmail.com>
2022-08-20 22:12:29 +0800

    style: fix tailwind styles not applied, add font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,20 +12,31 @@ export default function Home() {
         <title>Ahoy! Speedback ⚡️</title>
         <meta
           name="description"
-          content="Easiest way to conduct Speedback for your team."
+          content="The easiest way to conduct Speedback for your team."
         />
         <meta
           name="keywords"
           content="team, feedback, speedback, collaboration, retro, agile"
         />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=DynaPuff:wght@400;600&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <main className="p-4 h-full">
         <div className="grid place-items-center w-full">
-          <h1 className="text-2xl text-center">Pirates assemble!</h1>
+          <h1 className="text-3xl text-center font-dynapuff">Ahoy!</h1>
         </div>
         {currentUser ? (
           <div className="w-1/2 h-full">
+            <p className="text-2xl text-center">Pirates assemble!</p>
             <div className="h-1/4 m-4 items-center flex flex-col p-4">
               <h1 className="text-2xl">Are you a captain?</h1>
               <Button href="/ship/create">Create a Ship</Button>
@@ -34,7 +45,6 @@ export default function Home() {
             <div className="h-1/4 m-4 items-center flex flex-col p-4">
               <h1 className="text-2xl">Are you a crew?</h1>
               <Button href="/ship/join">Join a Ship</Button>
-              <Link href="/room">Join back a call</Link>
             </div>
           </div>
         ) : (
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   darkMode: 'class',
   content: [
     './node_modules/flowbite-react/**/*.js',
-    './pages/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './src/pages/*.{js,ts,jsx,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        dynapuff: ['DynaPuff', 'cursive']
+      }
+    },
   },
   plugins: [require('flowbite/plugin')],
 };
