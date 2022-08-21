bf26b1066fdd3a8168c5e7d3f21beb9c22d2f644
lyqht <estee.tsw@gmail.com>
2022-08-21 10:52:06 +0800

    style: styled homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Login from '@/components/Login';
 import { useUser } from '@supabase/auth-helpers-react';
 import { Button } from 'flowbite-react';
 import Head from 'next/head';
+import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Home() {
@@ -9,7 +10,7 @@ export default function Home() {
   return (
     <div className="h-full">
       <Head>
-        <title>Ahoy! Speedback ⚡️</title>
+        <title>Speedback Ahoy!</title>
         <meta
           name="description"
           content="The easiest way to conduct Speedback for your team."
@@ -30,26 +31,59 @@ export default function Home() {
           rel="stylesheet"
         />
       </Head>
-      <main className="p-4 h-full">
-        <div className="grid place-items-center w-full">
-          <h1 className="text-3xl text-center font-dynapuff">Ahoy!</h1>
-        </div>
-        {currentUser ? (
-          <div className="w-1/2 h-full">
-            <p className="text-2xl text-center">Pirates assemble!</p>
-            <div className="h-1/4 m-4 items-center flex flex-col p-4">
-              <h1 className="text-2xl">Are you a captain?</h1>
-              <Button href="/ship/create">Create a Ship</Button>
-              <Link href="/ship/join">Join back a Ship</Link>
-            </div>
-            <div className="h-1/4 m-4 items-center flex flex-col p-4">
-              <h1 className="text-2xl">Are you a crew?</h1>
-              <Button href="/ship/join">Join a Ship</Button>
-            </div>
+      <main className="h-full justify-between flex flex-col">
+        <div id="main-content" className="p-8">
+          <div className="grid place-items-center w-full">
+            <h1 className="text-6xl text-center font-dynapuff">Ahoy!</h1>
+            <p className="text-2xl text-center">Pirates assemble 🏴‍☠️</p>
           </div>
-        ) : (
-          <Login />
-        )}
+          <div className="grid place-items-center p-4">
+            {currentUser ? (
+              <div className="w-3/4 h-full items-center flex-row flex justify-evenly align-top">
+                <div className="h-1/4 m-4 items-center flex flex-col p-4 justify-start">
+                  <h1 className="text-2xl font-dynapuff pb-4">
+                    Are you a captain?
+                  </h1>
+                  <Button size={'xl'} href="/ship/create">
+                    Create a Ship
+                  </Button>
+                  <Link href="/ship/join">
+                    <p className="text-sm italic hover:text-blue-400 hover:cursor-pointer">
+                      Join back a Ship
+                    </p>
+                  </Link>
+                </div>
+                <div className="h-1/4 m-4 items-center flex flex-col p-4 justify-start">
+                  <h1 className="text-2xl font-dynapuff pb-4">
+                    Are you a crew?
+                  </h1>
+                  <Button size={'xl'} href="/ship/join">
+                    Join a Ship
+                  </Button>
+                </div>
+              </div>
+            ) : (
+              <Login />
+            )}
+          </div>
+        </div>
+        <div className="w-full relative">
+          <img
+            id="bottom-background"
+            alt="Background waves"
+            className="w-full bg-cover opacity-60 -z-20"
+            src={'/bottom_waves.svg'}
+          />
+          <p className="font-dynapuff absolute bottom-0 right-0 text-white text-lg p-4">
+            Made by{' '}
+            <a
+              className="underline decoration-white decoration-4 underline-offset-4"
+              href={'https://github.com/lyqht/'}
+            >
+              @lyqht
+            </a>
+          </p>
+        </div>
       </main>
     </div>
   );
